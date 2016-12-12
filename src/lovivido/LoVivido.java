afa7b2ba45fbb0d8c42b1846329c8b99abdcd4db
klemavilanova <klemavilanova@victoria18.danielcastelao.org>
2016-12-12 09:00:33 +0100

    Extraemos los metodos de calcular y pedir datos

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -8,28 +8,30 @@ import javax.swing.JOptionPane;
 
 public class LoVivido {
 
-public static void mostrarDatos(int m, int d, int h, String n) {
- JOptionPane.showMessageDialog(null,  "Meses: "  +m,"Numero de meses vividos de " +n,JOptionPane.INFORMATION_MESSAGE);
- JOptionPane.showMessageDialog(null, "Días: " +d,"Numero de días vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
- JOptionPane.showMessageDialog(null, "Horas: " +h,"Numero de horas vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
-}
+    static String n, e;
+    static int a, m, d, h;
 
-public static void main(String[] args) {
+    public static void mostrarDatos(int m, int d, int h, String n) {
+        JOptionPane.showMessageDialog(null, "Meses: " + m, "Numero de meses vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Días: " + d, "Numero de días vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Horas: " + h, "Numero de horas vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+    }
+    public static void pedirDatos() {
+        n = JOptionPane.showInputDialog("Escriba su nombre: ");
+        e = JOptionPane.showInputDialog("Escriba su edad: ");
+    }
+    public static void calcular() {
+        a = Integer.parseInt(e);
+        m = (a * 12);
+        d = (a * 365);
+        h = (d * 24);
+    }
 
-String n = JOptionPane.showInputDialog ("Escriba su nombre: ");
-String e = JOptionPane.showInputDialog ("Escriba su edad: ");
- 
-int a = Integer.parseInt (e);
-
-int m = (a*12);
-int d = (a*365);
-int h = (d*24);
-
-LoVivido.mostrarDatos(m, d, h, n);
-
-   System.exit (0);
-
-}
-   
+    public static void main(String[] args) {
+        LoVivido.pedirDatos();
+        LoVivido.calcular();
+        LoVivido.mostrarDatos(m, d, h, n);
+        System.exit(0);
+    }
 
 }
