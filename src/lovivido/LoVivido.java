3f690e1402a163527f7649cd195870e2d318ec78
klemavilanova <klemavilanova@victoria18.danielcastelao.org>
2016-12-12 08:54:37 +0100

    Extraccion del metodo mostrar datos

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -8,24 +8,24 @@ import javax.swing.JOptionPane;
 
 public class LoVivido {
 
+public static void mostrarDatos(int m, int d, int h, String n) {
+ JOptionPane.showMessageDialog(null,  "Meses: "  +m,"Numero de meses vividos de " +n,JOptionPane.INFORMATION_MESSAGE);
+ JOptionPane.showMessageDialog(null, "Días: " +d,"Numero de días vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
+ JOptionPane.showMessageDialog(null, "Horas: " +h,"Numero de horas vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
+}
+
 public static void main(String[] args) {
 
 String n = JOptionPane.showInputDialog ("Escriba su nombre: ");
 String e = JOptionPane.showInputDialog ("Escriba su edad: ");
  
 int a = Integer.parseInt (e);
- 
+
 int m = (a*12);
 int d = (a*365);
 int h = (d*24);
- 
- JOptionPane.showMessageDialog(null,  "Meses: "  +m,"Numero de meses vividos de " +n,JOptionPane.INFORMATION_MESSAGE);
 
-
- JOptionPane.showMessageDialog(null, "Días: " +d,"Numero de días vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
-
-
- JOptionPane.showMessageDialog(null, "Horas: " +h,"Numero de horas vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
+LoVivido.mostrarDatos(m, d, h, n);
 
    System.exit (0);
 
